34273aa8162914aeb490b81667b28a1c572f9567
acomble <antoine.comble@gmail.com>
2014-11-27 15:07:45 +0100

    Heure avec timezone correct + format français

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/notification/NotificationTemplateRenderer.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/notification/NotificationTemplateRenderer.java
@@ -20,7 +20,8 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.Serializable;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 /**
@@ -28,56 +29,57 @@ import java.util.Map;
  */
 public class NotificationTemplateRenderer {
 
-	public static String render(
-			NotificationTemplateContext notificationTemplateContext,
-			NotificationField notificationField)
-		throws Exception {
+	private static final SimpleDateFormat	SDF_DATE	= new SimpleDateFormat("EEEE dd MMMM yyyy");
+	private static final SimpleDateFormat	SDF_HOUR	= new SimpleDateFormat("HH");
+	private static final SimpleDateFormat	SDF_MINUTE	= new SimpleDateFormat("mm");
 
-		CalendarNotificationTemplate calendarNotificationTemplate =
-			notificationTemplateContext.getCalendarNotificationTemplate();
-		NotificationType notificationType =
-			notificationTemplateContext.getNotificationType();
-		NotificationTemplateType notificationTemplateType =
-			notificationTemplateContext.getNotificationTemplateType();
+	public static String render(NotificationTemplateContext notificationTemplateContext, NotificationField notificationField) throws Exception {
 
-		String notificationTemplate = NotificationUtil.getTemplate(
-			calendarNotificationTemplate, notificationType,
-			notificationTemplateType, notificationField);
+		CalendarNotificationTemplate calendarNotificationTemplate = notificationTemplateContext.getCalendarNotificationTemplate();
+		NotificationType notificationType = notificationTemplateContext.getNotificationType();
+		NotificationTemplateType notificationTemplateType = notificationTemplateContext.getNotificationTemplateType();
+
+		String notificationTemplate = NotificationUtil.getTemplate(calendarNotificationTemplate, notificationType, notificationTemplateType, notificationField);
 
 		return replaceTokens(notificationTemplate, notificationTemplateContext);
 	}
 
-	protected static String replaceTokens(
-			String notificationTemplate,
-			NotificationTemplateContext notificationTemplateContext)
-		throws Exception {
+	protected static String replaceTokens(String notificationTemplate, NotificationTemplateContext notificationTemplateContext) throws Exception {
 
-		Map<String, Serializable> attributes =
-			notificationTemplateContext.getAttributes();
+		Map<String, Serializable> attributes = notificationTemplateContext.getAttributes();
+
+		final Date endDate = (Date) attributes.get("endDate");
+		final Date startDate = (Date) attributes.get("startDate");
 
 		return StringUtil.replace(
-			notificationTemplate,
-			new String[] {
-				"[$EVENT_END_DATE$]", "[$EVENT_LOCATION$]",
-				"[$EVENT_START_DATE$]", "[$EVENT_TITLE$]", "[$EVENT_URL$]",
-				"[$FROM_ADDRESS$]", "[$FROM_NAME$]", "[$PORTAL_URL$]",
-				"[$PORTLET_NAME$]", "[$TO_ADDRESS$]", "[$TO_NAME$]"
-			},
-			new String[] {
-				GetterUtil.getString(attributes.get("endTime")),
-				GetterUtil.getString(attributes.get("location")),
-				GetterUtil.getString(attributes.get("startTime")),
-				GetterUtil.getString(attributes.get("title")),
-				GetterUtil.getString(attributes.get("url")),
-				GetterUtil.getString(
-					notificationTemplateContext.getFromAddress()),
-				GetterUtil.getString(notificationTemplateContext.getFromName()),
-				GetterUtil.getString(attributes.get("portalURL")),
-				GetterUtil.getString(attributes.get("portletName")),
-				GetterUtil.getString(
-					notificationTemplateContext.getToAddress()),
-				GetterUtil.getString(notificationTemplateContext.getToName())
-			});
+				notificationTemplate,
+				new String[] { 
+						"[$EVENT_END_DATE$]", 
+						"[$EVENT_LOCATION$]", 
+						"[$EVENT_START_DATE$]", 
+						"[$EVENT_TITLE$]", 
+						"[$EVENT_URL$]", 
+						"[$FROM_ADDRESS$]", 
+						"[$FROM_NAME$]", 
+						"[$PORTAL_URL$]",
+						"[$PORTLET_NAME$]", 
+						"[$TO_ADDRESS$]", 
+						"[$TO_NAME$]" 
+				},
+				new String[] { 
+						GetterUtil.getString(SDF_DATE.format(endDate) + " ра " + SDF_HOUR.format(endDate) + "h" + SDF_MINUTE.format(endDate)), 
+						GetterUtil.getString(attributes.get("location")),
+						GetterUtil.getString(SDF_DATE.format(startDate) + " ра " + SDF_HOUR.format(startDate) + "h" + SDF_MINUTE.format(startDate)), 
+						GetterUtil.getString(attributes.get("title")),
+						GetterUtil.getString(attributes.get("url")), 
+						GetterUtil.getString(notificationTemplateContext.getFromAddress()),
+						GetterUtil.getString(notificationTemplateContext.getFromName()), 
+						GetterUtil.getString(attributes.get("portalURL")), 
+						GetterUtil.getString(attributes.get("portletName")),
+						GetterUtil.getString(notificationTemplateContext.getToAddress()), 
+						GetterUtil.getString(notificationTemplateContext.getToName()) 
+				}
+			);
 	}
 
 }
